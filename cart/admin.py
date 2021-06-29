f713fbf82cd5570271c6b81a549d7a452406f7ac
apnafolio <apnafolio@gmail.com>
2021-06-29 16:44:58 +0530

    freshskinny update

---
+++ cart/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from cart.models import Cart,CartItem
+# Register your models here.
+class CartItemAdmin(admin.ModelAdmin):
+    list_display=('product','cart','quantity','is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'cart', 'quantity', 'is_active')
+
+class CartAdmin(admin.ModelAdmin):
+    list_display=('cart_id','date_added')
+    
+admin.site.register(Cart,CartAdmin)
+admin.site.register(CartItem,CartItemAdmin)
--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 # Register your models here.
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug':('product_name',)
     }
     list_display=('product_name','price','stock','category','modified_date','is_available')
-admin.site.register(Product,ProductsAdmin)
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','variation_value','created_date','is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product','variation_category','variation_value','created_date','is_active')
+
+
+admin.site.register(Product, ProductsAdmin)
+admin.site.register(Variation,VariationAdmin)
